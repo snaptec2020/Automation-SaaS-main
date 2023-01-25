01b8e14cfdfaf2da5790dd8339e7d595679f54cf
abedalaziz94 <121605397+abedalaziz94@users.noreply.github.com>
2023-01-25 14:36:09 +0300

    Create initial Script
    create initial script

---
+++ Object Repository/Headers and Footers/Web Header.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Web Header</name>
+   <tag></tag>
+   <elementGuidId>9727175f-1e7c-4943-8e8f-31a2b7400e2e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;main-header&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
