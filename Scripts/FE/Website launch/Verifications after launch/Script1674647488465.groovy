4a8dfaab02eb392e3fee4494715b57cf6e23cd9f
abedalaziz94 <121605397+abedalaziz94@users.noreply.github.com>
2023-01-25 14:59:30 +0300

    Commit a new contents
    Commit a new contents

--- Scripts/FE/Website launch/Verifications after launch/Script1674647488465.groovy
+++ Scripts/FE/Website launch/Verifications after launch/Script1674647488465.groovy
@@ -17,11 +17,3 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl(GlobalVariable.URL)
-
-WebUI.verifyElementVisible(findTestObject('Headers and Footers/Header contents/Web Header'))
-
-WebUI.verifyElementVisible(findTestObject('Headers and Footers/Web footer'))
-
