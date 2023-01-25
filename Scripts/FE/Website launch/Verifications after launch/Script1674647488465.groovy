0e448b545b065d52a73e052a94295abd2fe95c3c
AhmadMushet <121230871+AhmadMushet@users.noreply.github.com>
2023-01-25 17:25:52 +0300

    ST-1412 Add a new comment in test case

--- Scripts/FE/Website launch/Verifications after launch/Script1674647488465.groovy
+++ Scripts/FE/Website launch/Verifications after launch/Script1674647488465.groovy
@@ -17,3 +17,4 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+//We want to add all check after Launch here
