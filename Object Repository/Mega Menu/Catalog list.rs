7dee77555e4079e745e78d57bf27f774df9d0fc0
RazanAlhamad <r.bawaneh@snaptec.co>
2023-01-29 17:55:21 +0300

    Products and Catalog Keywords
    adding the following : 1-product list keyword
    catlog list keyword
    add to cart happay scenario

---
+++ Object Repository/Mega Menu/Catalog list.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Catalog list</name>
+   <tag></tag>
+   <elementGuidId>5e1c995b-56c9-4883-83bf-802157454696</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class=&quot;styles_megaMenuItem__v14fq  styles_underlineHighlight__yK8MD&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Products/SKU status.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>in stock or out of stock</description>
+   <name>SKU status</name>
+   <tag></tag>
+   <elementGuidId>220e5b15-9d95-47b0-9007-88b7ad348849</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@class='sku__status'][text()=&quot;متوفرة&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
