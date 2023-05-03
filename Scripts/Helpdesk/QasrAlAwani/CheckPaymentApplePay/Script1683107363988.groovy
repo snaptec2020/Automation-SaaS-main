394f53af0996934c56831a4ae45d6987853fb985
PC Mahmoud <PC Mahmoud@Mahmoud.home>
2023-05-03 12:50:38 +0300

    st-2073

--- Scripts/Helpdesk/QasrAlAwani/CheckPaymentApplePay/Script1683107363988.groovy
+++ Scripts/Helpdesk/QasrAlAwani/CheckPaymentApplePay/Script1683107363988.groovy
@@ -17,19 +17,3 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
-WebUI.maximizeWindow()
-
-WebUI.navigateToUrl(GlobalVariable.FE_URL)
-
-CustomKeywords.'products.productsFromCatalog.getRandominStockProductsFromRandomCategory'()
-
-WebUI.openBrowser('')
-
-WebUI.maximizeWindow()
-
-WebUI.navigateToUrl(GlobalVariable.FE_URL)
-
-CustomKeywords.'products.productsFromCatalog.getRandominStockProductsFromRandomCategory'()
-
