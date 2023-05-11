b951efcf1d4c5b20af1cb4ed8ab6b7b2734e70cf
Mahmoud Alyasein <mahmoud@snaptec.co>
2023-05-11 12:44:06 +0300

    st-2073

--- Test Suites/Helpdesk/AlAseel.groovy
+++ Test Suites/Helpdesk/AlAseel.groovy
@@ -31,17 +31,19 @@ import com.kms.katalon.core.annotation.TearDownTestCase
 /**
  * Setup test suite environment.
  */
-@SetUp(skipped = true) // Please change skipped to be false to activate this method.
+@SetUp(skipped = false) // Please change skipped to be false to activate this method.
 def setUp() {
 	// Put your code here.
+	WebUI.disableSmartWait()
 }
 
 /**
  * Clean test suites environment.
  */
-@TearDown(skipped = true) // Please change skipped to be false to activate this method.
+@TearDown(skipped = false) // Please change skipped to be false to activate this method.
 def tearDown() {
 	// Put your code here.
+	WebUI.enableSmartWait()
 }
 
 /**
