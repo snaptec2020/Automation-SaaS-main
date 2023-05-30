97944d00bf00d7bf36463a5088b99d8eb621e37a
abedalaziz94 <121605397+abedalaziz94@users.noreply.github.com>
2023-05-30 17:01:08 +0300

    ST-2119
    add cancel tabby by phone and add needQTY directly

---
+++ Object Repository/Cart/insert NeedQTY By text.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>insert NeedQTY By text</name>
+   <tag></tag>
+   <elementGuidId>2d8f21e2-5fdf-4fe2-bd49-594af8e1fc81</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='styles_quantityContainer__vpJfV']//input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
