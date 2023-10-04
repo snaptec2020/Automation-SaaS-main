f17dec9ca4e84fed1f05be7e88605fbde696a20e
AhmadMushet <121230871+AhmadMushet@users.noreply.github.com>
2023-10-04 18:14:01 +0300

    ST-modifying the Add location

--- Scripts/FE/Website launch/Validations/Add locatin New workflow/Script1693496571587.groovy
+++ Scripts/FE/Website launch/Validations/Add locatin New workflow/Script1693496571587.groovy
@@ -15,7 +15,9 @@ import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
+import internal.GlobalVariable
+
+import org.apache.commons.lang3.StringUtils
 import org.openqa.selenium.Keys as Keys
 import java.text.Collator as Collator
 import java.util.*
@@ -28,104 +30,111 @@ import java.util.*
  * 
  * int mapWidth = WebUI.getElementWidth(findTestObject('Object Repository/CheckOut/Map Block')) / 2
  */
-//WebUI.waitForElementClickable(findTestObject('Map Objs/Pick from map btn'), 10)
+//
 String script = 'return (JSON.parse(localStorage.getItem(\'BROWSER_PERSISTENCE__store_view_code\')).value).replaceAll(\'"\',\'\')'
 
 def store = WebUI.executeJavaScript(script, null)
 
 script = (('return JSON.parse(JSON.parse(localStorage.getItem(\'persist:availableCacheData:' + store) + '\')).appState).storeConfig.default_country_code')
 
-def countryCode = WebUI.executeJavaScript(script, null)
+//def countryCode = WebUI.executeJavaScript(script, null)
 
 //KeywordUtil.logInfo(countryCode.toString())
-Locale locale = new Locale('en', countryCode)
+def countryResponse = CustomKeywords.'generalactions.generalStrings.jsonParser'(WS.sendRequestAndVerify(findTestObject('APIs/Postman/Get country', [('URL') : GlobalVariable.URL, ('store') : store])).getResponseText())
+def locale = (countryResponse.data.countries[0].full_name_english).toString().toLowerCase()//new Locale('en', countryCode)
 
 
 
 //WebUI.click(findTestObject('Map Objs/Pick from map btn'))
-
 //WebUI.delay(50)
 //WebUI.setText(findTestObject('Map Objs/Input search location'), GlobalVariable.Countries[locale.getDisplayCountry().toLowerCase()])
-
 //WebUI.click(findTestObject('Map Objs/Select 1st location'))
-
 //WebUI.clickOffset(findTestObject('Map Objs/Map Block'), 0, 0)
-
-
 /*if (WebUI.waitForElementClickable(findTestObject('Map Objs/Continue After select Address'), 5)) {
     WebUI.click(findTestObject('Map Objs/Continue After select Address')) //def searchText = WebUI.getText(findTestObject('Map Objs/Map Title')).replaceAll('^.*,', '').toString().trim()
 
 } else {*/
-    script = (('return JSON.parse(JSON.parse(localStorage.getItem(\'persist:availableCacheData:' + store) + '\')).appState).locatorZones')
+//script = (('return JSON.parse(JSON.parse(localStorage.getItem(\'persist:availableCacheData:' + store) + '\')).appState).locatorZones')
 
-    def locatorZones = WebUI.executeJavaScript(script, null)
+String awsAccessKey
 
-    String awsAccessKey
+String awsSecretKey
 
-    String awsSecretKey
+String SecurityToken
 
-    String SecurityToken
+String regionName = 'eu-west-1'
 
-    String regionName = 'eu-west-1'
+if (WebUI.waitForElementClickable(findTestObject('Map Objs/Pick from map btn'), 5)) {
+	def locatorZonesResponse =CustomKeywords.'generalactions.generalStrings.jsonParser'(WS.sendRequestAndVerify(findTestObject('APIs/Postman/Get locator Zones', [('URL') : GlobalVariable.URL, ('store') : store])).getResponseText())//WebUI.executeJavaScript(script, null)
+	def locatorZones = locatorZonesResponse.data.getLocatorZones
+	KeywordUtil.logInfo(locatorZones.toString())
+    WebUI.click(findTestObject('Map Objs/Pick from map btn'))
 
-    if (locatorZones != null) {
-		WebUI.click(findTestObject('Map Objs/Pick from map btn'))
-        def latLong = []
+    def latLong = []
 
-        for (int i = 0; i <= (locatorZones.coordinate.size() - 1); i++) {
-            (locatorZones.coordinate[i]).each({ 
-                    latLong.add([it.latitude, it.longitude])
-                })
-        }
-        
-       for (int i = 0; i <= (latLong.size() - 1); i++) {
-            def resopnse = CustomKeywords.'generalactions.generalStrings.jsonParser'(WS.sendRequest(findTestObject('APIs/Location APIs/Postman/cognito-identity.eu-west-1.amazonaws.com')).getResponseText())
+    for (int i = 0; i <= (locatorZones.size() - 1); i++) {
+        (locatorZones[i]).each({ 
+			for (int j = 0; j <= (locatorZones.coordinate.size() - 1); j++) {
+				locatorZones.coordinate[j].each { 
+					latLong.add([it.latitude, it.longitude])
+					 }
+				}
+                
+            })
+    }
+    
+    for (int i = 0; i <= (latLong.size() - 1); i++) {
+        def resopnse = CustomKeywords.'generalactions.generalStrings.jsonParser'(WS.sendRequest(findTestObject('APIs/Location APIs/Postman/cognito-identity.eu-west-1.amazonaws.com')).getResponseText())
 
-            awsAccessKey = resopnse.Credentials.AccessKeyId
+        awsAccessKey = resopnse.Credentials.AccessKeyId
 
-            awsSecretKey = resopnse.Credentials.SecretKey
+        awsSecretKey = resopnse.Credentials.SecretKey
 
-            SecurityToken = resopnse.Credentials.SessionToken
+        SecurityToken = resopnse.Credentials.SessionToken
 
-            def locations = CustomKeywords.'com.amazonaws.services.s3.sample.getAPIResults.getAPIResultsByLatLong'(awsAccessKey, 
-                awsSecretKey, SecurityToken, regionName, ((latLong[i])[1]).toString(), ((latLong[i])[0]).toString())
+        def locations = CustomKeywords.'com.amazonaws.services.s3.sample.getAPIResults.getAPIResultsByLatLong'(awsAccessKey, 
+            awsSecretKey, SecurityToken, regionName, ((latLong[i])[1]).toString(), ((latLong[i])[0]).toString())
 
-            for (int j = 0; j <= (locations.size() - 1); j++) {
-                WebUI.setText(findTestObject('Map Objs/Input search location'), locations[j])
+        for (int j = 0; j <= (locations.size() - 1); j++) {
+            WebUI.setText(findTestObject('Map Objs/Input search location'), locations[j])
 
-                WebUI.click(findTestObject('Map Objs/Select 1st location'))
-
-                if (WebUI.waitForElementClickable(findTestObject('Map Objs/Continue After select Address'), 5)) {
-                    WebUI.click(findTestObject('Map Objs/Continue After select Address'))
-
-                    return null 
-                }
-            }
-        }
-    } else {
-		WebUI.click(findTestObject('Map Objs/Change address button'))
-		WebUI.click(findTestObject('Map Objs/Pick from map btn'))
-        int numTrails = 0
-		
-		WebUI.clickOffset(findTestObject('Map Objs/Map Block'), 0, 0)
-        while (!(WebUI.waitForElementClickable(findTestObject('Map Objs/Continue After select Address'), 5)) && (numTrails < 5)) {
-            numTrails++
-
-			WebUI.setText(findTestObject('Map Objs/Input search location'), GlobalVariable.Countries[locale.getDisplayCountry().toLowerCase()])
-
-			WebUI.delay(2)
             WebUI.click(findTestObject('Map Objs/Select 1st location'))
 
-            WebUI.doubleClick(findTestObject('Map Objs/Zoom In'))
+            if (WebUI.waitForElementClickable(findTestObject('Map Objs/Continue After select Address'), 5)) {
+                WebUI.click(findTestObject('Map Objs/Continue After select Address'))
 
-            WebUI.delay(2)
-
-            WebUI.clickOffset(findTestObject('Map Objs/Map Block'), 0, 0)
+                return null
+            }
         }
-        
-        WebUI.click(findTestObject('Map Objs/Continue After select Address'))
     }
-//}
+} else {
+    WebUI.click(findTestObject('Map Objs/Change address button'))
 
-//WebUI.callTestCase(findTestCase('FE/Website launch/Validations/Address Info Dialog'), [:], FailureHandling.STOP_ON_FAILURE)
+    WebUI.click(findTestObject('Map Objs/Pick from map btn'))
+
+    int numTrails = 0
+
+    WebUI.clickOffset(findTestObject('Map Objs/Map Block'), 0, 0)
+
+    while (!(WebUI.waitForElementClickable(findTestObject('Map Objs/Continue After select Address'), 5)) && (numTrails < 
+    5)) {
+        numTrails++
+
+        WebUI.setText(findTestObject('Map Objs/Input search location'), GlobalVariable.Countries[locale])
+
+        WebUI.delay(2)
+
+        WebUI.click(findTestObject('Map Objs/Select 1st location'))
+
+        WebUI.doubleClick(findTestObject('Map Objs/Zoom In'))
+
+        WebUI.delay(2)
+
+        WebUI.clickOffset(findTestObject('Map Objs/Map Block'), 0, 0)
+    }
+    
+    WebUI.click(findTestObject('Map Objs/Continue After select Address'))
+}
+
+
 
