924fd473376a187a5e39b0b78848beb14fc8d0bc
AhmadMushet <121230871+AhmadMushet@users.noreply.github.com>
2023-11-21 14:37:52 +0300

    ST-5391 update sign up xpath

--- Object Repository/Sign up Page/Mobile Web/SignUp Button.rs
+++ Object Repository/Sign up Page/Mobile Web/SignUp Button.rs
@@ -6,12 +6,11 @@
    <elementGuidId>9cc60b9b-103c-4576-b718-c9064dfc08ad</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value></value>
+         <key>BASIC</key>
+         <value>//div[normalize-space()='التسجيل' or normalize-space()='Sign Up']</value>
       </entry>
       <entry>
-         <key>BASIC</key>
-         <value>//div[normalize-space()='اشتراك' or normalize-space()='Sign Up']</value>
+         <key>XPATH</key>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -21,7 +20,15 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
+      <value>//div[normalize-space()='التسجيل' or normalize-space()='Sign Up']</value>
+      <webElementGuid>7c3e58c9-eb3c-4902-b7e9-227867bbcd5e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpathTest</name>
+      <type>Main</type>
       <value>//div[normalize-space()='اشتراك' or normalize-space()='Sign Up']</value>
-      <webElementGuid>8f933210-99bf-4101-80d4-382ceff5ea00</webElementGuid>
+      <webElementGuid>c4e1b842-67a8-43b2-a9d6-d9519b79b3c5</webElementGuid>
    </webElementProperties>
 </WebElementEntity>
