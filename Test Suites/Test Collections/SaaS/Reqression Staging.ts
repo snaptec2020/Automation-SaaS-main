936182712f250c8d02832778d47fdb6f0019dd7a
AhmadMushet <121230871+AhmadMushet@users.noreply.github.com>
2024-01-09 14:15:33 +0300

    ST-5391 ST-3424 Commit to test

---
+++ Test Suites/Test Collections/SaaS/Reqression Staging.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>Reqression Staging</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Custom</groupName>
+            <profileName>SaaS-Stg-Tanmiah</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>iphone 6-7-8 plus</runConfigurationId>
+         </configuration>
+         <runEnabled>false</runEnabled>
+         <testSuiteEntity>Test Suites/Test Suites/FE/Regression Suites/Sign In - Up suites</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Custom</groupName>
+            <profileName>SaaS-Stg-Tanmiah</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>iphone 6-7-8 plus</runConfigurationId>
+         </configuration>
+         <runEnabled>false</runEnabled>
+         <testSuiteEntity>Test Suites/Test Suites/FE/Regression Suites/Regression test after deployments - AR</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Custom</groupName>
+            <profileName>SaaS-Stg-Tanmiah</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>iphone 6-7-8 plus</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Test Suites/FE/Regression Suites/Checkout All methods</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
