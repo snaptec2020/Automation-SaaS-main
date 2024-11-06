d816f8626ccb99d0c8048437ec6efa4522158a7c
AhmadMushet <121230871+AhmadMushet@users.noreply.github.com>
2024-11-06 16:20:17 +0300

    Change the normal behavior to depends on the current location

---
+++ Object Repository/Map Objs/Current Location.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Current Location</name>
+   <tag></tag>
+   <elementGuidId>21951ebb-3fbc-4fb9-bb19-6b9da76b41d9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[starts-with(@class,'styles_deliveryLocation__')]/span[starts-with(@class,'styles_label__')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[starts-with(@class,'styles_deliveryLocation__')]/span[starts-with(@class,'styles_label__')]</value>
+      <webElementGuid>15e532b0-9ffa-4f49-8188-6d7a8026a334</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
