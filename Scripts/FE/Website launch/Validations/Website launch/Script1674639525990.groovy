62130d81446591a762d2c66cfd3b03824a35ce62
omar-snaptec <omar.salem@snaptec.co>
2024-12-01 10:52:57 +0300

    update Maxmize Window on website launch

--- Scripts/FE/Website launch/Validations/Website launch/Script1674639525990.groovy
+++ Scripts/FE/Website launch/Validations/Website launch/Script1674639525990.groovy
@@ -11,36 +11,43 @@ import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject
-import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+
 //KeywordUtil.logInfo(RunConfiguration.getExecutionProfile())
 //Map geoLocationOption =[:]
 //geoLocationOption.put("latitude",21.3369007)
 //geoLocationOption.put("longitude",39.1291145)
 //RunConfiguration.setWebDriverPreferencesProperty('Emulation.setGeolocationOverride', geoLocationOption)
-switch(GlobalVariable.launchingConfig.get("Mode")) {
-	case 'FE': WebUI.openBrowser('')
-				
-			   WebUI.maximizeWindow()
+switch (GlobalVariable.launchingConfig.get('Mode')) {
+    case 'FE':
+        WebUI.openBrowser('')
 
-			   WebUI.navigateToUrl(GlobalVariable.URL)
+        WebUI.navigateToUrl(GlobalVariable.URL)
 
-			   CustomKeywords.'generalactions.generalActions.waiteSpinnerToHide'()
-			   WebUI.callTestCase(findTestCase('FE/Website launch/Validations/Add locatin New workflow'), [:], FailureHandling.STOP_ON_FAILURE)
-			   break;
-	case 'BE': WebUI.openBrowser('')
+        CustomKeywords.'generalactions.generalActions.waiteSpinnerToHide'()
 
-			   WebUI.maximizeWindow()
-			   
-			   switch(GlobalVariable.launchingConfig.get("BEMode")) {
-				   case 'MID': WebUI.navigateToUrl(GlobalVariable.MID_URL.get(GlobalVariable.launchingConfig.get("SiteUrl")))
-				   			   break
-				   
-			   }
+        WebUI.callTestCase(findTestCase('FE/Website launch/Validations/Add locatin New workflow'), [:], FailureHandling.STOP_ON_FAILURE)
+
+        break
+    case 'BE':
+        WebUI.openBrowser('')
+
+        switch (GlobalVariable.launchingConfig.get('BEMode')) {
+            case 'MID':
+                WebUI.navigateToUrl(GlobalVariable.MID_URL.get(GlobalVariable.launchingConfig.get('SiteUrl')))
+
+                break
+        }
 }
-GlobalVariable.RunningMode=CustomKeywords.'generalactions.EnvironmentSettings.isRunningByMobile'()
+
+GlobalVariable.RunningMode = CustomKeywords.'generalactions.EnvironmentSettings.isRunningByMobile'()
+
+if (GlobalVariable.RunningMode in [1,2]) {
+    WebUI.maximizeWindow(FailureHandling.CONTINUE_ON_FAILURE)
+}
