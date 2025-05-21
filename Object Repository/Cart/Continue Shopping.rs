bd81d6fc7fca859da422add42d9595483c886aaa
omar-snaptec <omar.salem@snaptec.co>
2025-05-21 15:56:08 +0300

    update selector

--- Object Repository/Cart/Continue Shopping.rs
+++ Object Repository/Cart/Continue Shopping.rs
@@ -6,13 +6,14 @@
    <elementGuidId>487020d1-7f20-451f-baf5-5f3818e4999d</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//div[@class=&quot;add-to-cart-modal&quot;]//button[@class='button button--white button--medium']</value>
+         <key>BASIC</key>
       </entry>
       <entry>
-         <key>BASIC</key>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;add-to-cart-modal&quot;]//button[@class=&quot;button button--white button--medium&quot;]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>false</useRalativeImagePath>
 </WebElementEntity>
--- Object Repository/Products/Product container in page.rs
+++ Object Repository/Products/Product container in page.rs
@@ -6,14 +6,14 @@
    <elementGuidId>5bc079dd-e31e-4861-bd4f-d8c02f2b2b63</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//*[contains(text(),'أضف إلى السلة') or contains(text(),'Add to cart')]/parent::div
-</value>
+         <key>BASIC</key>
       </entry>
       <entry>
-         <key>BASIC</key>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'styles_productItem_')][.//span[contains(@class, 'styles_regularPrice') or contains(@class,'styles_oldPrice__')]/span[@dir]/span/span[2][not(text()='0')]]//button[contains(text(),'Add to Cart') or contains(text(),'أضف إلى السلة')]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>false</useRalativeImagePath>
 </WebElementEntity>
